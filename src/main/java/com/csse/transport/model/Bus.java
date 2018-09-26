e3dc037dcf5468617abc8e1435189b1b884ba034
nadun94 <nadunsirimevan@gmail.com>
2018-09-27 00:23:12 +0530

    push to get repo by 12.23

--- src/main/java/com/csse/transport/model/Bus.java
+++ src/main/java/com/csse/transport/model/Bus.java
@@ -1,16 +1,54 @@
 package com.csse.transport.model;
 
+import org.hibernate.annotations.Fetch;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-//@Entity
+@Entity
 public class Bus {
 
-//    @Id
+  @Id
     private String BusID;
     private String make;
     private String type;
-//    private Route route;
 
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "RouteID",nullable = false)
+    private Route route;
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public String getBusID() {
+        return BusID;
+    }
+
+    public void setBusID(String busID) {
+        BusID = busID;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/com/csse/transport/service/CordinateMath.java
+++ src/main/java/com/csse/transport/service/CordinateMath.java
@@ -1,5 +1,10 @@
 package com.csse.transport.service;
 
+import com.csse.transport.model.Bus;
+import com.csse.transport.model.BusStop;
+
+import java.util.List;
+
 public class CordinateMath {
 
     public  double calcDistance(double lat1, double lon1, double lat2,double lon2) {
@@ -19,5 +24,31 @@ public class CordinateMath {
 //        distance = Math.pow(distance, 2) + Math.pow(height, 2);
 
         return distance;
+    }
+
+    public BusStop findNearestHalt(List<BusStop> busHalts,double cLat,double cLon){
+
+        double temp_distance,min_distance;
+//        String passengerHalt;
+        BusStop bs,bus_stop;
+
+        bus_stop=busHalts.get(0);
+//        passengerHalt= busHalts.get(0).getBusHaltName();
+        min_distance = calcDistance(cLat,cLon,busHalts.get(0).getBusHaltLatitude(),busHalts.get(0).getBusHaltLongitude());
+
+        for(int i =0;i<busHalts.size();i++){
+            bs = busHalts.get(i);
+            temp_distance = calcDistance(cLat,cLon,bs.getBusHaltLatitude(),bs.getBusHaltLongitude());
+            if(temp_distance>min_distance)
+                continue;
+            else{
+                min_distance = temp_distance;
+                bus_stop= bs;
+//                passengerHalt = bs.getBusHaltName();
+            }
+
+
+        }
+        return bus_stop;
     }
 }
