49380d781491ccb4b0a8ac9ff1407899b3a3bf23
C_Jay <chandirajayasekara@gmail.com>
2018-10-05 20:22:32 +0530

    User logging backend Completed

--- src/main/java/com/csse/transport/controller/LoginController.java
+++ src/main/java/com/csse/transport/controller/LoginController.java
@@ -12,16 +12,93 @@ import java.util.Map;
 @RequestMapping(path="/login")
 public class LoginController {
 
+    public class LogedUser{
+        private String pid;
+        private String name;
+        private String emial;
+        private String accLevel;
+        private Boolean auth;
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getPid() {
+            return pid;
+        }
+
+        public void setPid(String pid) {
+            this.pid = pid;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmial() {
+            return emial;
+        }
+
+        public void setEmial(String emial) {
+            this.emial = emial;
+        }
+
+        public String getAccLevel() {
+            return accLevel;
+        }
+
+        public void setAccLevel(String accLevel) {
+            this.accLevel = accLevel;
+        }
+
+        public Boolean getAuth() {
+            return auth;
+        }
+
+        public void setAuth(Boolean auth) {
+            this.auth = auth;
+        }
+    }
+
     @Autowired
     private LoginRepository loginRepository;
 
     @PostMapping(path="/log")
-    public @ResponseBody Passenger login (@RequestBody Map<String, String> body){
-        String pid = body.get("pid");
-//        System.out.println(pid);
-        String password = body.get("password");
-//        System.out.println(loginRepository.findByPidAndPassword(pid, password));
+    public @ResponseBody LogedUser login (@RequestBody Map<String, String> body){
+        LogedUser lg =  new LogedUser();
+        try {
+            String pid = body.get("pid");
+            String password = body.get("password");
+            Passenger ps = loginRepository.findByPidAndPassword(pid, password);
+            System.out.println(ps);
+            if(ps != null){
+                lg.setPid(ps.getPid());
+                lg.setName(ps.getName());
+                lg.setEmial(ps.getEmail());
+                lg.setAccLevel(ps.getAccLevel());
+                lg.setMessage("User Found");
+                lg.setAuth(true);
+            }
+            System.out.println(ps.getSex());
+            System.out.println(lg.getName());
 
-        return loginRepository.findByPidAndPassword(pid, password);
+        }catch (Exception ex){
+            System.out.println(ex);
+
+            lg.setMessage("No User Found!!!");
+            lg.setAuth(false);
+        }
+
+
+        return lg;
     }
 }
